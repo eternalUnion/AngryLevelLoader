21549b2c2b509bb20fa52ad740190825a4f822e6
eternalUnion <dnzbyk12345@gmail.com>
2023-06-30 14:57:09 +0200

    Fixed first door not lighting up before opening

--- RudeLevelScripts/FirstRoomSpawner.cs
+++ RudeLevelScripts/FirstRoomSpawner.cs
@@ -33,38 +33,6 @@ namespace RudeLevelScript
 				mainCam.backgroundColor = backgroundColor;
 				mainCam.clearFlags = cameraFillMode;
 
-				// Fix first room door not having yellow texture while opening
-				if (finalDoor.allRenderers == null)
-					finalDoor.allRenderers = finalDoor.GetComponentsInChildren<MeshRenderer>();
-
-				foreach (MeshRenderer mr in finalDoor.allRenderers)
-				{
-					if (mr.sharedMaterial == null)
-						continue;
-
-					Material instMat = mr.sharedMaterial;
-					string realName = instMat.name;
-					while (realName.EndsWith(" (Instance)"))
-						realName = realName.Substring(0, realName.Length - " (Instance)".Length);
-					
-					int index = -1;
-					for (int i = 0; i < finalDoor.offMaterials.Length; i++)
-					{
-						if (finalDoor.offMaterials[i].name == realName)
-						{
-							index = i;
-							break;
-						}
-					}
-
-					Debug.Log($"{mr.sharedMaterial.name} : {realName} : {index}");
-
-					if (index != -1)
-					{
-						finalDoor.offMaterials[index] = mr.sharedMaterial;
-					}
-				}
-
 				FinalDoorOpener opener = firstRoomInst.transform.Find("Room/FinalDoor/FinalDoorOpener").GetComponent<FinalDoorOpener>();
 				opener.startMusic = startMusic;
 				FinalDoor door = firstRoomInst.transform.Find("Room/FinalDoor").GetComponent<FinalDoor>();
