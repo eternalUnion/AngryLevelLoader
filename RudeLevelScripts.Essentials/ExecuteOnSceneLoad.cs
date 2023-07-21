9a8742eeaccf9a1fee8550b59cd2b7ad9204db0c
eternalUnion <dnzbyk12345@gmail.com>
2023-07-22 02:39:03 +0300

    Added tooltips

--- RudeLevelScripts.Essentials/ExecuteOnSceneLoad.cs
+++ RudeLevelScripts.Essentials/ExecuteOnSceneLoad.cs
@@ -8,6 +8,7 @@ namespace RudeLevelScripts.Essentials
 {
 	public class ExecuteOnSceneLoad : MonoBehaviour
 	{
+		[Tooltip("Lower value ExecuteOnSceneLoad are executed first")]
 		public int relativeExecutionOrder = 0;
 		public UnityEvent onSceneLoad;
 
