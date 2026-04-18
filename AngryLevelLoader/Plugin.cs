d8ffb0a4038c10f17f4a57ee41a1961a544eccdc
eternalUnion <dnzbyk12345@gmail.com>
2026-04-18 11:39:15 +0200

    Version bump

--- AngryLevelLoader/Plugin.cs
+++ AngryLevelLoader/Plugin.cs
@@ -72,7 +72,7 @@ namespace AngryLevelLoader
 	{
         public const string PLUGIN_NAME = "AngryLevelLoader";
         public const string PLUGIN_GUID = "com.eternalUnion.angryLevelLoader";
-        public const string PLUGIN_VERSION = "3.2.2";
+        public const string PLUGIN_VERSION = "3.2.3";
 
 		public const string PLUGIN_CONFIG_MIN_VERSION = "1.8.0";
 
