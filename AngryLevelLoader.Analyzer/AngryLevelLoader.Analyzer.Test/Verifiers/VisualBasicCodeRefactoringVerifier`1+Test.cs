7972d45034468b7d8239e2fa2c8d0d6e64e798f7
eternalUnion <dnzbyk12345@gmail.com>
2026-05-01 00:44:42 +0200

    Added back unit test verifiers

---
+++ AngryLevelLoader.Analyzer/AngryLevelLoader.Analyzer.Test/Verifiers/VisualBasicCodeRefactoringVerifier`1+Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace AngryLevelLoader.Analyzer.Test
+{
+    public static partial class VisualBasicCodeRefactoringVerifier<TCodeRefactoring>
+        where TCodeRefactoring : CodeRefactoringProvider, new()
+    {
+        public class Test : VisualBasicCodeRefactoringTest<TCodeRefactoring, MSTestVerifier>
+        {
+        }
+    }
+}
