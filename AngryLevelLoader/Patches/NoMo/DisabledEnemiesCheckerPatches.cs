f4f9f774116a5af2dfaf1d049f8597d8ab3a3e44
eternalUnion <dnzbyk12345@gmail.com>
2026-07-07 17:18:10 +0300

    Added patch for disabled enemies checker to be triggered when playing with NoMo enabled

---
+++ AngryLevelLoader/Patches/NoMo/DisabledEnemiesCheckerPatches.cs
@@ -0,0 +1,29 @@
+﻿using AngryLevelLoader.Managers;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ULTRAKILL.Cheats;
+
+namespace AngryLevelLoader.Patches.NoMo
+{
+	[HarmonyPatch(typeof(DisabledEnemiesChecker))]
+	internal class DisabledEnemiesCheckerPatches
+	{
+		[HarmonyPatch(nameof(DisabledEnemiesChecker.Update))]
+		[HarmonyPrefix]
+		public static bool Update(DisabledEnemiesChecker __instance)
+		{
+			if (!AngrySceneManager.isInCustomLevel || !AngryGamemodeManager.NoMonsters)
+				return true;
+
+			if (!__instance.activated && StatsManager.Instance && StatsManager.Instance.levelStarted)
+			{
+				__instance.activated = true;
+				__instance.Invoke("Activate", __instance.delay);
+			}
+
+			return false;
+		}
+	}
+}
